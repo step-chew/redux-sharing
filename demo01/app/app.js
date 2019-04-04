c10da6527ac9c74e2b05e25f60148091fa381c14
Stephen <github@step.chew.es>
2019-04-05 00:35:37 +0800

    Complete vanilla redux

--- demo01/app/app.js
+++ demo01/app/app.js
@@ -1,13 +1,42 @@
 const { createStore } = Redux;
 
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    case 'COMPUTE':
+      return state + action.value;
+  }
+  return state;
+};
+
 const store = createStore(counter);
 
+store.subscribe(() => {
+  document.getElementById('result').innerText = store.getState();
+});
 
 document.getElementById('btnIncrement').addEventListener('click', () => {
-
+  store.dispatch({
+    type: 'INCREMENT'
+  });
 });
 
 document.getElementById('btnDecrement').addEventListener('click', () => {
-
+  store.dispatch({
+    type: 'DECREMENT'
+  });
 });
 
+document.getElementById('btnCompute').addEventListener('click', () => {
+  let val;
+
+  if (!isNaN(val = parseInt(document.querySelector('input[name="figure"]').value, 10))) {
+    store.dispatch({
+      type: 'COMPUTE',
+      value: val
+    });
+  }
+});
