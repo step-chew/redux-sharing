84f3a763690bb38c28561132426fb9f4ee938a32
Stephen Chew <pww@topdanmark.dk>
2019-04-04 18:46:23 +0800

    Skeleton

---
+++ app/app.js
@@ -0,0 +1,13 @@
+const { createStore } = Redux;
+
+const store = createStore(counter);
+
+
+document.getElementById('btnIncrement').addEventListener('click', () => {
+
+});
+
+document.getElementById('btnDecrement').addEventListener('click', () => {
+
+});
+
